f61038cf8ea0bda149c2c029a6e612044aa80643
Amit Cohen <acohen@outbrain.com>
2022-05-28 17:09:21 +0300

    Initial Commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+let request = require('supertest');
+let app = require('../index.js');
+
+describe('GET /', function() {
+    it('when navigating to root path, app responds with text hello world', function(done) {
+        // navigate to root and check that the response is "hello world"
+        request(app).get('/').expect('hello world', done);
+    });
+});
